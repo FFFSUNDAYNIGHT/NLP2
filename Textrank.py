1c47d5cb484794deec61186340cd1ddb009d5c78
lihuichao <lihuichaolihc@gmail.com>
2018-01-27 12:30:16 +0800

    将节点信息转换为位置信息

--- Textrank.py
+++ Textrank.py
@@ -4,50 +4,56 @@ import networkx as nx
 
 class rank:
     def __init__(self):
-        pass
+        self.sentens = []
 
     def Addnode(self, doc, G):
-    '''
-    Add node to the graph according to the doc's sentences
 
-    :param doc: The doc need to cope with
-    :param G: The origin graph
-    :return: The graph with nodes added
-    '''
+        '''
+        Add node to the graph according to the doc's sentences
 
+        :param doc: The doc need to cope with
+        :param G: The origin graph
+        :return: The graph with nodes added
+        '''
         with open(doc, 'r', encoding='utf8') as f:
             content = f.read().replace('\ufeff','')
             content = content.replace('\n','')
-            sentens = content.split('。')
-        
-        for sente in sentens:
-            G.add_node(sente)
-        G.remove_node('')
+            self.sentens = content.split('。')
+
+        for i in range(len(self.sentens) - 1):
+            G.add_node(i)
         return G
 
-    def Addedge(self, G, threshold):
-    
-    '''
-    Add edges to the graph
+    def Addedge(self, G, threshold = 0.01):
 
-    :param G: The graph need to add edges
-    :param threshold: Deciding whether a edge should be added
-    :return: The graph with edges added
-    '''
+        '''
+        Add edges to the graph
+        
+        :param G: The graph need to add edges
+        :param threshold: Deciding whether a edge should be added
+        :return: The graph with edges added
+        '''
         for sentelead in G:
             for senteflo in G:
-                if simi(sentelead, senteflo) > threshold and sentelead != senteflo:
-                    G.add_edge(sentelead, senteflo, weight = simi(sentelead, senteflo))
+                if simi(self.sentens[sentelead], self.sentens[senteflo]) > threshold and sentelead != senteflo:
+                    G.add_edge(sentelead, senteflo, weight = simi(self.sentens[sentelead], self.sentens[senteflo]))
         return G
+    def docsummary(self, doc, threshold = 0.01, Keynum = 3):
+        G = nx.Graph()
+        self.Addnode(doc, G)
+        self.Addedge(G, 0.01)
+        G_pagerank = pagerank(G)
+         # Find out the most important sentences
+        keyphrase = sorted(G_pagerank, key=G_pagerank.get, reverse=True)
+        try:
+            for i in range(Keynum):
+                print(self.sentens[keyphrase[i]])
+        except IndexError:
+            print('The doc must have more than three sentences')
 if __name__ == '__main__':
-    G = nx.Graph()
     ranking = rank()
-    ranking.Addnode('test.txt', G)
-    ranking.Addedge(G, 0.01)
-    G_pagerank = pagerank(G)
-    # Find out the most important sentences
-    keyphrase = sorted(G_pagerank, key=G_pagerank.get, reverse=True)
-    print(keyphrase[0],keyphrase[1],keyphrase[2])
+    ranking.docsummary('test1.txt')
+
 
         
 
